47ba7e07b640c6599ac18125b83c4f5fad4ed69b
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-06 14:41:21 +0600

    add set time out

---
+++ set-time.js
@@ -0,0 +1,11 @@
+setTimeout(function () {
+    console.log('I am coading javaScript');
+}, 3500)
+
+setTimeout(function () {
+    console.log('I am verry happy to my web learning');
+}, 3000)
+
+setTimeout(function () {
+    console.log('I am verry Excited learning web development');
+}, 2500)
